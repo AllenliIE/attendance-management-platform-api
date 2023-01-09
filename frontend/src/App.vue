789068a3c550bd034154bd42ff5854fad1d7cb9a
AllenliIE <allen.li.service@gmail.com>
2023-01-10 04:33:01 +0800

    feat: create frontend file

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="attendance-management-platform-api">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default {
+  name: "App",
+};
+</script>
