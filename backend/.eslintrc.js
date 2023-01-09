b3b9cd2511e7064168e2fbfcb0f4f7eaed8debe3
AllenliIE <allen.li.service@gmail.com>
2023-01-10 03:32:47 +0800

    feat: create backend file

---
+++ backend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["vue"],
+  rules: {},
+};
