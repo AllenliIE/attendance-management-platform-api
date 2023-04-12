023805bc03071ec6cc89dfcc4dc93b2a67ff0648
AllenliIE <allen.li.service@gmail.com>
2023-04-12 19:55:06 +0800

    feat: replace getToken in all APIs with axios interceptors

--- frontend/src/apis/attendance.js
+++ frontend/src/apis/attendance.js
@@ -1,23 +1,10 @@
 import { apiHelper } from "./../utils/helpers";
-const getToken = () => localStorage.getItem("token");
 
 export default {
   postAttendance({ UserId, date, clockIn }) {
-    return apiHelper.post(
-      "/attendance",
-      { UserId, date, clockIn },
-      {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      }
-    );
+    return apiHelper.post("/attendance", { UserId, date, clockIn });
   },
   updateAttendance({ UserId, date, clockOut }) {
-    return apiHelper.put(
-      `/attendance/${UserId}`,
-      { UserId, date, clockOut },
-      {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      }
-    );
+    return apiHelper.put(`/attendance/${UserId}`, { UserId, date, clockOut });
   },
 };
